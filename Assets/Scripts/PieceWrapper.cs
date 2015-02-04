c912bd4625546d4c279369fa8fb3177ee69a9041
Gkxd <dhua@andrew.cmu.edu>
2015-02-04 17:01:13 -0500

    Simplified rotation code

--- Assets/Scripts/PieceWrapper.cs
+++ Assets/Scripts/PieceWrapper.cs
@@ -3,107 +3,100 @@ using System.Collections;
 using System.Collections.Generic;
 
 [System.Serializable]
-public class Serializable2DIntArray
-{
-		public int[] array;
+public class Serializable2DIntArray {
+    public int[] array;
 }
 
-public class PieceWrapper : MonoBehaviour
-{
+public class PieceWrapper : MonoBehaviour {
 
-		public Color color;
-		public Material[] tileSprites;
-		public Serializable2DIntArray[] serializablePieceNumbers;
-		private Piece piece;
-		private GameObject numberContainer;
-		public GameObject centerCollider;
-		public GameObject edgesCollider;
-		bool move = false;
-	public int pieceSize = 9;
-	GameObject[] numbers;
+    public Color color;
+    public Material[] tileSprites;
+    public Serializable2DIntArray[] serializablePieceNumbers;
+    private Piece piece;
+    private GameObject numberContainer;
+    private CircleCollider2D edgeCollider;
+    bool move = false;
 
+    void Start() {
+        piece = Piece.fromSerializable2DIntArray(serializablePieceNumbers);
 
-		void Start ()
-		{
-				piece = Piece.fromSerializable2DIntArray (serializablePieceNumbers);
+        // Adding tiles in a wrapper object
+        numberContainer = new GameObject("numbers");
+        numberContainer.transform.parent = transform;
 
-				// Adding tiles in a wrapper object
-				numberContainer = new GameObject ("numbers");
-				numberContainer.transform.parent = transform;
-		numbers = new GameObject[pieceSize];
+        int[,] pieceNumbers = piece.to2DArray();
+        int count = 0;
 
-				int[,] pieceNumbers = piece.to2DArray ();
-				int count = 0;
-				for (int i = 0; i < pieceNumbers.GetLength(0); i++) {
-						for (int j = 0; j < pieceNumbers.GetLength(1); j++) {
-								// Create quads for non-zero numbers
-								if (pieceNumbers [i, j] >= 1 && pieceNumbers [i, j] <= 9) {
-										GameObject quad = GameObject.CreatePrimitive (PrimitiveType.Quad);
-										quad.transform.parent = numberContainer.transform;
+        Vector3 centerOffset = new Vector3(pieceNumbers.GetLength(0)/4.0f, -pieceNumbers.GetLength(1)/2.0f, 0);
 
-										quad.transform.localPosition = new Vector3 (j, -i, 0);
-										// Set the texture to the corresponding number
-										quad.renderer.material = tileSprites [pieceNumbers [i, j] - 1];
-										quad.renderer.material.color = color;
-										
+        for (int i = 0; i < pieceNumbers.GetLength(0); i++) {
+            for (int j = 0; j < pieceNumbers.GetLength(1); j++) {
+                // Create quads for non-zero numbers
+                if (pieceNumbers[i, j] >= 1 && pieceNumbers[i, j] <= 9) {
+                    GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    quad.transform.parent = numberContainer.transform;
 
-										quad.AddComponent<ForceTileUpright> ();
-										quad.collider.isTrigger = true;
-										quad.layer = 2;
-										numbers [count] = quad;
-										count ++;
-								}
-						}
-				}
-				
+                    quad.transform.localPosition = new Vector3(j, -i, 0) - centerOffset;
+                    // Set the texture to the corresponding number
+                    quad.renderer.material = tileSprites[pieceNumbers[i, j] - 1];
+                    quad.renderer.material.color = color;
+                                        
 
-				// instantiates the center and edges collider with same transform coordiantes as this parent piece
-				
-				GameObject temp;
-				temp = GameObject.Instantiate (centerCollider, transform.position, Quaternion.identity) as GameObject; 
-				temp.transform.position = new Vector3 (temp.transform.position.x, temp.transform.position.y, -2.0f);
-				temp = GameObject.Instantiate (edgesCollider, transform.position, Quaternion.identity) as GameObject;
-				temp.transform.position = new Vector3 (temp.transform.position.x, temp.transform.position.y, 0.0f);
-				move = false;
-		}
+                    quad.AddComponent<ForceTileUpright>();
+                    quad.collider.isTrigger = true;
+                    quad.layer = 2;
+                    count ++;
+                }
+            }
+        }
+                
+
+        // Instantiates the center and edges collider at the center of the parent piece
+
+        edgeCollider = gameObject.AddComponent<CircleCollider2D>();
+        edgeCollider.radius = 1;
+        move = false;
+    }
     
-		void Update ()
-		{
-				rotateClockwise ();
-		}
-	
-		void rotateClockwise ()
-		{
-				if (Input.GetMouseButtonDown (0)) {
-						RaycastHit hit; 
-						Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-						if (Physics.Raycast (ray, out hit, 20.0f)) {
+    void Update() {
+        handleInput();
+    }
 
-								if (hit.collider.CompareTag ("Edges") && hit.collider.transform.position.x == transform.position.x &&
-										hit.collider.transform.position.y == transform.position.y) {
-										// rotate piece clockwise
-										Debug.Log ("Rotate");
-										transform.Rotate (new Vector3 (0.0f, 0.0f, 90.0f));
-										foreach (GameObject number in numbers) {
-												number.transform.Rotate (new Vector3 (0.0f, 0.0f, -90.0f));
-										}
-								}
-						}  
-				} 
-		}
-	
-		void rotateCounterClockwise ()
-		{
+    void OnMouseOver() {
+        Debug.Log("hello");
+        if (Input.GetMouseButtonDown(0)) { // Left Click
+            rotateClockwise();
+        }
+        if (Input.GetMouseButtonDown(1)) { // Right Click
+            rotateCounterClockwise();
+        }
+    }
 
-		}
-	
-		void clickAndDrag ()
-		{
-				
-		}
+    void handleInput() {
+        // Placeholder controls
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            rotateClockwise();
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            rotateCounterClockwise();
+        }
+    }
+    
+    void rotateClockwise() {
+        transform.Rotate(0, 0, 90);
+        piece.rotateClockwise();
+    }
+    
+    void rotateCounterClockwise() {
+        transform.Rotate(0, 0, -90);
+        piece.rotateCounterClockwise();
+    }
+    
+    void clickAndDrag() {
+                
+    }
 
-		public Piece getPiece ()
-		{
-				return piece;
-		}
+    public Piece getPiece() {
+        return piece;
+    }
 }
