203e48d40aa97029357745e4d76b187a308c78ec
Roy <rikkyroy@gmail.com>
2015-02-08 18:12:16 -0500

    Added algorithm script for random piece generation, using new scene for testing

---
+++ Assets/Scripts/PuzzleDatabase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleDatabase : MonoBehaviour {
+	
+	public string[] puzzle;								// prefab puzzles
+	
+	public string SelectPuzzle(){
+		string code = "";										// puzzle code
+		if(puzzle.Length > 0){
+			int index = Random.Range(0,puzzle.Length);					// random puzzle number
+			code = puzzle[index];																	// select puzzle
+		}
+		return code;																						// return puzzle
+	}
+}
