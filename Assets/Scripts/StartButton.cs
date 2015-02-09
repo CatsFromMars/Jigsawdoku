2273b33a5d4a4643c68f17ad7625ae82e9f4fe30
Madeline Duque <mduque@andrew.cmu.edu>
2015-02-08 21:39:21 -0500

    Working Menu

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : _PressableButton {
+	
+	public Material normalSprite;
+	public Material hoverSprite;
+	public Material pressedSprite;
+	public int width = 512;
+	public int height = 164;
+	override
+	public Material getNormalSprite() {
+		transform.rotation = Quaternion.identity;
+		return normalSprite;
+	}
+	
+	override
+	public Material getHoverSprite() {
+		return hoverSprite;
+	}
+	
+	override
+	public Material getPressedSprite() {
+		return pressedSprite;
+	}
+	
+	override
+	public Vector2 getDimensions() {
+		return new Vector2(width, height);
+	}
+	
+	override
+	public float getScaleFactor() {
+		return 100;
+	}
+	
+	override
+	public void onButtonPressed() {
+		Application.LoadLevel("main_game");
+	}
+}
