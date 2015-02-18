7a58fcc8e843fdadc888f908a362c08d13b40a47
Jordan Tick <jrtick@andrew.cmu.edu>
2015-02-18 14:00:43 -0500

    further scoring update (it's cooler).

--- Assets/Timer/Timer.cs
+++ Assets/Timer/Timer.cs
@@ -4,18 +4,20 @@ using System.Collections;
 
 public class Timer : MonoBehaviour
 {
-	public float curTime;
 	public int score;
-	public int deltTime;
-	public int timeBonus;
-	public bool gameOver;
-	public Difficulty difficulty;
+	private float deltTime;
 	private float dif;
 	private int baseScore;
+	private float timeBonus;
+
+	public int updateTime;
+	public float curTime;
+	public bool gameOver;
+	public Difficulty difficulty;
 
 	Text text;
 
-	void Awake (){
+	void Start (){
 		text = GetComponent <Text> ();
 		curTime = 0;
 		switch (difficulty) {
@@ -48,11 +50,11 @@ public class Timer : MonoBehaviour
 			dif=0;
 			break;
 		}
-		score = baseScore;
+		score = 0;
 	}
 
 	void Update (){
-		if (!gameOver) {
+		if (!gameOver) { //just display time
 				curTime += Time.deltaTime;
 				int min = Mathf.FloorToInt (curTime) / 60;
 				int sec = Mathf.FloorToInt (curTime) % 60;
@@ -60,14 +62,18 @@ public class Timer : MonoBehaviour
 					text.text = min + ":0" + sec;
 				else
 					text.text = min + ":" + sec;
-		}else {
-			deltTime = Mathf.RoundToInt(timeBonus-curTime);
-			if(deltTime>0){
-				score=baseScore+Mathf.RoundToInt(deltTime/30)*(deltTime/30)*90;
-			}else{
-				score=baseScore;
+		}else { //display time on gameOver
+			if(score==0){ //only calculate score once
+				deltTime = (timeBonus-curTime); //timeBonus is max time allowed for a bonus
+				if(deltTime>0){ //if has a time bonus
+					//score=baseScore+((deltTime/30+1)*(deltTime/30+1))*90;
+					float temp = (float)((Mathf.CeilToInt(deltTime)/updateTime+1)*updateTime); //this makes it so score is equal for each "updateTime" interval
+					score=baseScore+Mathf.RoundToInt(((float)baseScore)*(temp*temp/timeBonus/timeBonus))/100*100; //ignores last two digits, quadratic decay
+				}else{ //if user runs out of time, no time bonus
+					score=baseScore;
+				}
 			}
-			text.text="Score: "+score;
+			text.text="Base score: "+baseScore+"\nTime Bonus: "+(score-baseScore)+"\nTotal: "+score;
 		}
 	}
 }
